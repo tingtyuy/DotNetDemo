4422f372070a3449a11bc7860efbca56e0fb3229
mengkai <363255751@qq.com>
2025-03-11 16:21:14 +0800

    clear

--- DotNetDemo/Program.cs
+++ DotNetDemo/Program.cs
@@ -1,9 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using DotNetDemo.DataTableModule;
 using Dumpify;
 using System.Data;
-
-var dtA=DataTableModule.CreateTable(null);
-var dtB=DataTableModule.CreateTable(null);
-dtA.Merge(dtB,false, MissingSchemaAction.AddWithKey);
-dtA.DumpConsole();
